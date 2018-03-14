fa8bc6bb9a41842f6d04d95b71fb38451fbc1080
Adler Zamora <a01630908@itesm.mx>
2018-03-13 22:09:25 -0600

    V2

--- client.js
+++ client.js
@@ -1,14 +1,15 @@
 var net = require("net");
-var fileOperator = require("./fileOperator.js");
+var FileOperator = require("./fileOperator.js");
 module.exports = class Client {
     constructor(host) {
         this.socket = new net.Socket();
         this.socket.connect(1113, host, () => {
             console.log("Connected");
         });
-        var fileOperator = new fileOperator();
+        var fileOperator = new FileOperator();
 
         this.socket.on("data", data => {
+            console.log(data.toString("utf-8"));
             var obj = JSON.parse(data.toString("utf-8"));
             if (Array.isArray(obj)) {
                 fileOperator.SaveFile(obj[0]);
